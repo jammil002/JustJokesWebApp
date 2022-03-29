8c8d1f3f5aae9c3560ca8576a065a1815b705c52
James Miller <jmiller25@my.gcu.edu>
2022-03-29 12:46:19 -0700

    Add project files.

---
+++ JustJokesWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JustJokesWebApp.Models;
+
+namespace JustJokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JustJokesWebApp.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ JustJokesWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JustJokesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
